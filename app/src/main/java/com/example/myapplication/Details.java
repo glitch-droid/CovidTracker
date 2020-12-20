2ff741afb4d6ecdee19fcc3ea251638bf33bdc90
Mriganka Bharali <mrigankabharali30@gmail.com>
2020-12-20 18:51:45 +0530

    Details PreFinal

--- app/src/main/java/com/example/myapplication/Details.java
+++ app/src/main/java/com/example/myapplication/Details.java
@@ -12,6 +12,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Details extends AppCompatActivity {
 
+    TextView cases, recovered, critical, deaths, tests, active;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +27,22 @@ public class Details extends AppCompatActivity {
                 .asBitmap()
                 .load(country.getFlag())
                 .into(flag);
+
+        cases = findViewById(R.id.cases);
+        critical = findViewById(R.id.critical);
+        active = findViewById(R.id.active);
+        recovered = findViewById(R.id.recovered);
+        deaths = findViewById(R.id.deaths);
+        tests = findViewById(R.id.tests);
+
+        setValues(country);
+    }
+    private void setValues(CountryModel mCountry){
+        cases.setText(mCountry.getCases());
+        critical.setText(mCountry.getCritical());
+        recovered.setText(mCountry.getRecovered());
+        tests.setText(mCountry.getTests());
+        active.setText(mCountry.getActive());
+        deaths.setText(mCountry.getDeaths());
     }
 }
